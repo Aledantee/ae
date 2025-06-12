f570836bac2620d0d9c4a10688be13f5f3ba8eac
Alexander tegtmeier <alexandertegtmeier@proton.me>
2025-06-12 11:02:27 +0200

    add Wrap

---
+++ utils.go
@@ -0,0 +1,22 @@
+package ae
+
+// Wrap creates a new error with the given message and wraps the provided errors as causes.
+// It filters out any nil errors from the provided list.
+// If all provided errors are nil, Wrap returns nil.
+// The returned error will have the given message and all non-nil errors as its causes.
+func Wrap(msg string, errs ...error) error {
+	var filtered []error
+	for _, err := range errs {
+		if err != nil {
+			filtered = append(filtered, err)
+		}
+	}
+
+	if len(filtered) == 0 {
+		return nil
+	}
+
+	return New().
+		Causes(filtered).
+		Msg(msg)
+}
